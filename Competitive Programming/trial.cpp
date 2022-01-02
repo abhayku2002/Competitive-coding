6c7b48e4ebc898f057f9844030e55ac93214cd78
Abhay kumar <abhayku2002@gmail.com>
2022-01-02 23:52:36 +0530

    Uploaded 2nd day of code

---
+++ Competitive Programming/trial.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    a=12;
+    cout<<"size of int"<<sizeof(a)<<endl;
+    float b;
+    b=32;
+    cout<<"size of float"<<sizeof(b)<<endl;
+    char c;
+    cout<<"size of char"<<sizeof(c)<<endl;
+    short int si;
+    cout<<"size of short int"<<sizeof(si)<<endl;
+    long int li;
+    cout<<"size of long int"<< sizeof(li)<<endl;
+    return 0;
+     
+}
