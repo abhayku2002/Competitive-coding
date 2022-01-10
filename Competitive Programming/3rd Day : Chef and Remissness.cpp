d748b1993fb6d4efa57967052f1855a1ff73e71c
Abhay kumar <abhayku2002@gmail.com>
2022-01-10 22:49:27 +0530

    Update 3rd Day : Chef and Remissness.cpp

--- Competitive Programming/3rd Day : Chef and Remissness.cpp
+++ Competitive Programming/3rd Day : Chef and Remissness.cpp
@@ -3,11 +3,11 @@ CHEF AND REMISSNESS:
 PROBLEM STATEMENT -
 
 
-The office where the Chef works, has two guards who count how many times a person enters into the office building. Though the duty of a guard is 24 hours in a day, sometimes they fall asleep during their duty and do not track the entry of a person in the office building. But one good thing is that they never fall asleep at the same time. At least one of them remains awake and counts who enters into the office.
-
-
-Now the boss of Chef wants to calculate how many times the Chef has entered into the building. The boss asked to the guard and they gave him two integers A and B, the count of first guard and second guard respectively.
-
+The office where the Chef works, has two guards who count how many times a person enters into the office building. 
+Though the duty of a guard is 24 hours in a day, sometimes they fall asleep during their duty and do not track the entry of a person in the office building.
+But one good thing is that they never fall asleep at the same time. At least one of them remains awake and counts who enters into the office.
+Now the boss of Chef wants to calculate how many times the Chef has entered into the building.
+The boss asked to the guard and they gave him two integers A and B, the count of first guard and second guard respectively.
 
 Help the boss to count the minimum and maximum number of times Chef could have entered into the office building.
 
