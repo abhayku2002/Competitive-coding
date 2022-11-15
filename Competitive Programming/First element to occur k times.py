c77d871e2cbc1ca6d235eb5b7e504f98c605bf57
Abhay kumar <abhayku2002@gmail.com>
2022-11-16 00:52:04 +0530

    Update First element to occur k times.py
    added solution

--- Competitive Programming/First element to occur k times.py
+++ Competitive Programming/First element to occur k times.py
@@ -27,3 +27,53 @@ Constraints:
 1 <= N, K <= 105
 1<= A <= 106
 '''
+#User function Template for python3
+
+
+class Solution:
+
+    def firstElementKTime(self,  a, n, k):
+
+        # code here
+
+        res={}
+
+        for ele in a:
+
+            if ele not in res:
+
+                res[ele]=0
+
+            res[ele]+=1
+
+            if res[ele]==k:
+
+                return ele
+
+        return -1
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+def main():
+
+    T = int(input())
+
+    while(T > 0):
+        sz = [int(x) for x in input().strip().split()]
+        n, k = sz[0], sz[1]
+        a = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(ob.firstElementKTime(a, n, k))
+
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
