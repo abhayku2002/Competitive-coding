3b75045b4fb37ac04ea5255282f5a6ffbb699cdb
Abhay kumar <abhayku2002@gmail.com>
2022-11-18 01:03:01 +0530

    Create Customers Who Never Order.sql

---
+++ MySQL/Customers Who Never Order.sql
@@ -0,0 +1,27 @@
+SQL Schema
+Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID and name of a customer.
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key column for this table.
+customerId is a foreign key of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+ 
+ 
+ 
