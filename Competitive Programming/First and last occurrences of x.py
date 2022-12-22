83c5869059d773d922192665eb2dda7c8f1dcb61
Abhay Kumar <abhayku2002@gmail.com>
2022-12-22 23:48:22 +0530

    Update First and last occurrences of x.py

--- Competitive Programming/First and last occurrences of x.py
+++ Competitive Programming/First and last occurrences of x.py
@@ -34,3 +34,36 @@ Constraints:
 
  '''
 
+#User function Template for python3
+
+
+def find(arr,n,x):
+    li=[]
+
+    for i in range(n):
+
+        if x == arr[i]:
+
+            p=i
+
+            li.append(x) 
+
+    if len(li)==0:
+
+        return -1,-1
+
+    return p+1-len(li),p
+
+
+
+#{ 
+ # Driver Code Starts
+t=int(input())
+for _ in range(0,t):
+    l=list(map(int,input().split()))
+    n=l[0]
+    x=l[1]
+    arr=list(map(int,input().split()))
+    ans=find(arr,n,x)
+    print(*ans)
+# } Driver Code Ends
