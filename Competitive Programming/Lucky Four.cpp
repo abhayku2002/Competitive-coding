ff6b0c4521e72586b89901589a52aac901b040e3
Abhay Kumar <abhayku2002@gmail.com>
2023-01-07 19:58:34 +0530

    Update Lucky Four.cpp

--- Competitive Programming/Lucky Four.cpp
+++ Competitive Programming/Lucky Four.cpp
@@ -1,1 +1,18 @@
+/*
+PROBLEM
+You are given a list of T integers, for each of them you have to calculate the number of occurrences of the digit 4 in the decimal representation.
+
+Input
+The first line of input consists of a single integer T, denoting the number of integers in the list.
+
+Then, there are T lines, each of them contain a single integer from the list.
+
+Output
+Output T lines. Each of these lines should contain the number of occurrences of the digit 4 in the respective integer from the list.
+
+Constraints
+1 ≤ T ≤ 105
+(Subtask 1): 0 ≤ Numbers from the list ≤ 9 - 33 points.
+(Subtask 2): 0 ≤ Numbers from the list ≤ 109 - 67 points.
+*/
 
