2813379bf237b49488dd660e8ea89a64bad7b32d
Abhay Kumar <abhayku2002@gmail.com>
2023-01-07 19:45:20 +0530

    Update First and Last Digit.cpp

--- Competitive Programming/First and Last Digit.cpp
+++ Competitive Programming/First and Last Digit.cpp
@@ -13,3 +13,27 @@ Constraints
 1  10001≤T≤1000
 2  10000001≤N≤1000000
 */
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main (){
+	ll t;
+	cin>> t;
+	while(t--){
+		ll n, first , last;
+		cin>>n;
+		last=n%10;
+		while(n>=10){
+			n=n/10;
+
+		}
+		first=n;
+		cout<< first+last;
+		cout<<endl;
+
+	}
+
+	return 0;
+	
+}
