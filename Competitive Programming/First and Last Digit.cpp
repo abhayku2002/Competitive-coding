9322a52f861034b5f8a0623f850214aabfa7dbf8
Abhay Kumar <abhayku2002@gmail.com>
2023-01-07 19:44:59 +0530

    Update First and Last Digit.cpp

--- Competitive Programming/First and Last Digit.cpp
+++ Competitive Programming/First and Last Digit.cpp
@@ -1,1 +1,15 @@
 
+/*
+Problem
+If Give an integer N . Write a program to obtain the sum of the first and last digits of this number.
+
+Input Format
+The first line contains an integer T, the total number of test cases. Then follow T lines, each line contains an integer N.
+
+Output Format
+For each test case, display the sum of first and last digits of N in a new line.
+
+Constraints
+1  10001≤T≤1000
+2  10000001≤N≤1000000
+*/
