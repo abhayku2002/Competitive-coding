cac5df7021cfac84204385a1c4e48bbb86c1d7e6
Abhay Kumar <abhayku2002@gmail.com>
2023-01-07 20:14:46 +0530

    Update Valid Triangles.cpp

--- Competitive Programming/Valid Triangles.cpp
+++ Competitive Programming/Valid Triangles.cpp
@@ -1,1 +1,14 @@
+/*
+Problem
+Write a program to check whether a triangle is valid or not, when the three angles of the triangle are the inputs. A triangle is valid if the sum of all the three angles is equal to 180 degrees.
+
+Input Format
+The first line contains an integer T, the total number of testcases. Then T lines follow, each line contains three angles A, B and C, of the triangle separated by space.
+
+Output Format
+For each test case, display 'YES' if the triangle is valid, and 'NO', if it is not, in a new line.
+
+Constraints
+1 ≤ T ≤ 1000 1 ≤ A,B,C ≤ 180
+*/
 
