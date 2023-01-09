8e8b5928bd1a0530842a1a474228c30276824648
Abhay Kumar <abhayku2002@gmail.com>
2023-01-09 20:06:22 +0530

    Create Instagram.cpp

---
+++ Competitive Programming/Instagram.cpp
@@ -0,0 +1,17 @@
+/*Problem
+Chef categorises an instagram account as spam, if, the following count of the account is more than 1010 times the count of followers.
+
+Given the following and follower count of an account as XX and YY respectively, find whether it is a spam account.
+
+Input Format
+The first line of input will contain a single integer TT, denoting the number of test cases.
+Each test case consists of two space-separated integers XX and YY — the following and follower count of an account, respectively.
+Output Format
+For each test case, output on a new line, YES, if the account is spam and NO otherwise.
+
+You may print each character of the string in uppercase or lowercase. For example, the strings YES, yes, Yes and yES are identical.
+
+Constraints
+1 \leq T \leq 1001≤T≤100
+1 \leq X, Y \leq 1001≤X,Y≤100
+*/
