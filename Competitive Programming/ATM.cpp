0b22914370b7e7bfa03fa9f7a4d046c31237ad0a
Abhay Kumar <abhayku2002@gmail.com>
2023-01-09 19:22:52 +0530

    Update ATM.cpp

--- Competitive Programming/ATM.cpp
+++ Competitive Programming/ATM.cpp
@@ -1,1 +1,19 @@
 
+/*
+Problem
+Pooja would like to withdraw X $US from an ATM. The cash machine will only accept the transaction if X is a multiple of 5, and Pooja's account balance has enough cash to perform the withdrawal transaction (including bank charges). For each successful withdrawal the bank charges 0.50 $US.
+
+Calculate Pooja's account balance after an attempted transaction.
+
+Input Format
+Each input contains 2 integers XX and YY.
+XX is the amount of cash which Pooja wishes to withdraw.
+YY is Pooja's initial account balance.
+
+Output Format
+Output the account balance after the attempted transaction, given as a number with two digits of precision. If there is not enough money in the account to complete the transaction, output the current bank balance.
+
+Constraints
+0 \lt X \leq 20000<X≤2000 - the amount of cash which Pooja wishes to withdraw.
+0 \leq Y \leq 20000≤Y≤2000 with two digits of precision - Pooja's initial account balance.
+*/
