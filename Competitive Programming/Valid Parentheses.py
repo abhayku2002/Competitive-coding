0a44282f5f4ecc9fb41959aeb6a5ccbd68871c8c
Abhay Kumar <abhayku2002@gmail.com>
2023-01-09 19:07:56 +0530

    Update Valid Parentheses.py

--- Competitive Programming/Valid Parentheses.py
+++ Competitive Programming/Valid Parentheses.py
@@ -28,3 +28,17 @@ Constraints:
 s consists of parentheses only '()[]{}'.
 '''
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        ack = []
+        lookfor = {')':'(', '}':'{', ']':'['}
+
+        for p in s:
+            if p in lookfor.values():
+                ack.append(p)
+            elif ack and lookfor[p] == ack[-1]:
+                ack.pop()
+            else:
+                return False
+
+        return ack == []
