ea217a123391fe14189d7d0a12777151d077cdf4
Abhay Kumar <abhayku2002@gmail.com>
2023-01-11 01:59:03 +0530

    Update Sum of Digits.cpp

--- Competitive Programming/Sum of Digits.cpp
+++ Competitive Programming/Sum of Digits.cpp
@@ -1,1 +1,15 @@
 
+/*
+Problem
+You're given an integer N. Write a program to calculate the sum of all the digits of N.
+
+Input Format
+The first line contains an integer T, the total number of testcases. Then follow T lines, each line contains an integer N.
+
+Output Format
+For each test case, calculate the sum of digits of N, and display it in a new line.
+
+Constraints
+1 \leq T \leq 10001≤T≤1000
+1 \leq N \leq 10000001≤N≤1000000
+*/
