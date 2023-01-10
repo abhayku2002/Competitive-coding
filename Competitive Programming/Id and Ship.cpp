a03ccf1a887036954c6e191c0f4578f312396732
Abhay Kumar <abhayku2002@gmail.com>
2023-01-10 23:59:19 +0530

    Update Id and Ship.cpp

--- Competitive Programming/Id and Ship.cpp
+++ Competitive Programming/Id and Ship.cpp
@@ -1,1 +1,19 @@
+/*
+Problem
+Write a program that takes in a letterclass ID of a ship and display the equivalent string class description of the given ID. Use the table below.
+
+Class ID	Ship Class
+B or b	BattleShip
+C or c	Cruiser
+D or d	Destroyer
+F or f	Frigate
+Input Format
+The first line contains an integer T, the total number of testcases. Then T lines follow, each line contains a character.
+
+Output Format
+For each test case, display the Ship Class depending on ID, in a new line.
+
+Constraints
+1 ≤ T ≤ 1000
+*/
 
