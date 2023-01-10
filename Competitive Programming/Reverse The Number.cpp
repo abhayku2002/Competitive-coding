47b751155bf88fc9b84615a03f6494c899dbf4e1
Abhay Kumar <abhayku2002@gmail.com>
2023-01-10 23:39:04 +0530

    Update Reverse The Number.cpp

--- Competitive Programming/Reverse The Number.cpp
+++ Competitive Programming/Reverse The Number.cpp
@@ -1,1 +1,15 @@
+/*
+Problem
+Given an Integer N, write a program to reverse it.
+
+Input
+The first line contains an integer T, total number of testcases. Then follow T lines, each line contains an integer N.
+
+Output
+For each test case, display the reverse of the given number N, in a new line.
+
+Constraints
+1 ≤ T ≤ 1000
+1 ≤ N ≤ 1000000
+*/
 
