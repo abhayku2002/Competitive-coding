777f2df8afeca2787ce8a90548a6e69ff4ffc825
Abhay Kumar <abhayku2002@gmail.com>
2023-01-11 00:04:47 +0530

    Update Find Remainder.cpp

--- Competitive Programming/Find Remainder.cpp
+++ Competitive Programming/Find Remainder.cpp
@@ -1,1 +1,15 @@
 
+/*
+Problem
+Write a program to find the remainder when an integer A is divided by an integer B.
+
+Input
+The first line contains an integer T, the total number of test cases. Then T lines follow, each line contains two Integers A and B.
+
+Output
+For each test case, find the remainder when A is divided by B, and display it in a new line.
+
+Constraints
+1 ≤ T ≤ 1000
+1 ≤ A,B ≤ 10000
+*/
