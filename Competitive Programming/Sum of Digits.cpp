35df92d774bfc790a0183163c45bec537aeccb6f
Abhay Kumar <abhayku2002@gmail.com>
2023-01-11 01:59:21 +0530

    Update Sum of Digits.cpp

--- Competitive Programming/Sum of Digits.cpp
+++ Competitive Programming/Sum of Digits.cpp
@@ -13,3 +13,26 @@ Constraints
 1 \leq T \leq 10001≤T≤1000
 1 \leq N \leq 10000001≤N≤1000000
 */
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int sum = 0;
+        while(n!=0)
+        {
+            int r = n%10;
+            sum+= r;
+            n /= 10;
+        }
+
+        cout<<sum<<endl;
+    }
+
+    return 0;
+}
