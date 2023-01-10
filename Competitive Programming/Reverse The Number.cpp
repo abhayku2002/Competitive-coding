2d6fd9a708c8bd886269ee8a21c6b5c0111592c2
Abhay Kumar <abhayku2002@gmail.com>
2023-01-10 23:39:15 +0530

    Update Reverse The Number.cpp

--- Competitive Programming/Reverse The Number.cpp
+++ Competitive Programming/Reverse The Number.cpp
@@ -13,3 +13,26 @@ Constraints
 1 ≤ N ≤ 1000000
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int ans=0;
+        while(n)
+        {
+            ans=ans*10+n%10;
+            n/=10;
+
+        }
+        cout<<ans<<endl;
+
+    }
+
+
+    return 0;
+}
