057cf4851c263ff21e8f55acf67046518c889c72
Abhay Kumar <abhayku2002@gmail.com>
2023-01-12 23:31:27 +0530

    Update Water Consumption.cpp

--- Competitive Programming/Water Consumption.cpp
+++ Competitive Programming/Water Consumption.cpp
@@ -1,1 +1,18 @@
 
+/*
+Problem
+Recently, Chef visited his doctor. The doctor advised Chef to drink at least 20002000 ml of water each day.
+
+Chef drank XX ml of water today. Determine if Chef followed the doctor's advice or not.
+
+Input Format
+The first line contains a single integer TT — the number of test cases. Then the test cases follow.
+The first and only line of each test case contains one integer XX — the amount of water Chef drank today.
+Output Format
+For each test case, output YES if Chef followed the doctor's advice of drinking at least 20002000 ml of water. Otherwise, output NO.
+
+You may print each character of the string in uppercase or lowercase (for example, the strings YES, yEs, yes, and yeS will all be treated as identical).
+
+Constraints
+1 \leq T \leq 20001≤T≤2000
+*/
