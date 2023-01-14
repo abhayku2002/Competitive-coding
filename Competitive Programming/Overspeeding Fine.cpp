9b03a8c93808e0d7cb5852d213f1316f979d38a5
Abhay Kumar <abhayku2002@gmail.com>
2023-01-15 00:30:58 +0530

    Update Overspeeding Fine.cpp

--- Competitive Programming/Overspeeding Fine.cpp
+++ Competitive Programming/Overspeeding Fine.cpp
@@ -1,1 +1,22 @@
+/*
+Problem
+Chef was driving on a highway at a speed of XX km/hour.
+
+To avoid accidents, there are fine imposed on overspeeding as follows:
+
+No fine if the speed of the car \leq 70≤70 km/hour.
+Rs 500500 fine if the speed of the car is strictly greater than 7070 and \leq 100≤100.
+Rs 20002000 fine if the speed of the car is strictly greater than 100100.
+Determine the fine Chef needs to pay.
+
+Input Format
+The first line of input will contain a single integer TT, denoting the number of test cases.
+Each test case consists of a single integer XX denoting the speed of Chef's car.
+Output Format
+For each test case, output the fine paid by Chef.
+
+Constraints
+1 \leq T \leq 2001≤T≤200
+1 \leq X \leq 2001≤X≤200
+*/
 
