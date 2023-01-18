79031c232eec464a8043aac9905c66dbc68eea88
Abhay Kumar <abhayku2002@gmail.com>
2023-01-18 20:24:06 +0530

    Update Mana points.cpp

--- Competitive Programming/Mana points.cpp
+++ Competitive Programming/Mana points.cpp
@@ -1,1 +1,20 @@
+/*
+Problem
+Chef is playing a mobile game. In the game, Chef's character Chefario can perform special attacks. However, one special attack costs XX mana points to Chefario.
+
+If Chefario currently has YY mana points, determine the maximum number of special attacks he can perform.
+
+Input Format
+The first line contains a single integer TT — the number of test cases. Then the test cases follow.
+The first and only line of each test case contains two space-separated integers XX and YY — the cost of one special attack and the number of mana points Chefario has initially.
+Output Format
+For each test case, output the maximum number of special attacks Chefario can perform.
+
+Constraints
+1 \leq T \leq 10^51≤T≤10 
+5
+ 
+1 \le X \le 1001≤X≤100
+1 \le Y \le 10001≤Y≤1000
+*/
 
