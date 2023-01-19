3963ca12983a7d7548141dd2e452ba430309784c
Abhay Kumar <abhayku2002@gmail.com>
2023-01-19 18:05:09 +0530

    Update Vaccine Dates.cpp

--- CodeChef/Vaccine Dates.cpp
+++ CodeChef/Vaccine Dates.cpp
@@ -1,1 +1,27 @@
 
+/*
+Problem
+Chef has taken his first dose of vaccine 
+�
+D days ago. He may take the second dose no less than 
+�
+L days and no more than 
+�
+R days since his first dose.
+
+Determine if Chef is too early, too late, or in the correct range for taking his second dose.
+
+Input Format
+First line will contain 
+�
+T, number of testcases. Then the testcases follow.
+Each testcase contains of a single line of input, three integers 
+�
+,
+�
+,
+�
+D,L,R.
+Output Format
+For each test case, print a single line containing one string - "Too Early" (without quotes) if it's too early to take the vaccine, "Too Late" (without quotes) if it's too late to take the vaccine, "Take second dose now" (without quotes) if it's the correct time to take the vaccine.
+*/
