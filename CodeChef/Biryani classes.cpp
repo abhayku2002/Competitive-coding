57359e45b7d0634afd7a36b396b86a2878c39fa6
Abhay Kumar <abhayku2002@gmail.com>
2023-01-25 23:54:03 +0530

    Update Biryani classes.cpp

--- CodeChef/Biryani classes.cpp
+++ CodeChef/Biryani classes.cpp
@@ -1,1 +1,24 @@
 
+/*
+Problem
+According to a recent survey, Biryani is the most ordered food. Chef wants to learn how to make world-class Biryani from a MasterChef. Chef will be required to attend the MasterChef's classes for 
+�
+X weeks, and the cost of classes per week is 
+�
+Y coins. What is the total amount of money that Chef will have to pay?
+
+Input Format
+The first line of input will contain an integer 
+�
+T — the number of test cases. The description of 
+�
+T test cases follows.
+The first and only line of each test case contains two space-separated integers 
+�
+X and 
+�
+Y, as described in the problem statement.
+Output Format
+For each test case, output on a new line the total amount of money that Chef will have to pay.
+
+*/
