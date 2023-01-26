e712c376284e2918dcf4d8592be730df33050741
Abhay Kumar <abhayku2002@gmail.com>
2023-01-27 00:58:11 +0530

    Update and rename Valid Parentheses.cpp to Valid Parentheses.py

--- LeetCode/Valid Parentheses.py
+++ LeetCode/Valid Parentheses.py
@@ -1,4 +1,4 @@
-/*
+'''
 Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 An input string is valid if:
@@ -6,4 +6,9 @@ An input string is valid if:
 Open brackets must be closed by the same type of brackets.
 Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
-*/
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while '()' in s or '[]'in s or '{}' in s:
+            s = s.replace('()','').replace('[]','').replace('{}','')
+        return False if len(s) !=0 else True
