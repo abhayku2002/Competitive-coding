fe05ab235da8cab9c5907018cd24d35d6b4b83fa
Abhay Kumar <abhayku2002@gmail.com>
2023-01-28 17:20:46 +0530

    Update Lucky Four.cpp

--- CodeChef/Lucky Four.cpp
+++ CodeChef/Lucky Four.cpp
@@ -1,1 +1,15 @@
 
+/*
+Problem
+Karan likes the number 4 very much.
+
+Impressed by the power of this number, Karan has begun to look for occurrences of four anywhere. He has a list of T integers, for each of them he wants to calculate the number of occurrences of the digit 4 in the decimal representation. He is too busy now, so please help him.
+
+Input Format
+The first line of input consists of a single integer T, denoting the number of integers in Karan's list.
+
+Then, there are T lines, each of them contain a single integer from the list.
+
+Output Format
+Output T lines. Each of these lines should contain the number of occurrences of the digit 4 in the respective integer from Karan's list.
+*/
