6525622d44cebb86369eb1672a47179b1b85d11a
Abhay Kumar <abhayku2002@gmail.com>
2023-01-28 17:10:49 +0530

    Update Adding Two Numbers.cpp

--- CodeChef/Adding Two Numbers.cpp
+++ CodeChef/Adding Two Numbers.cpp
@@ -1,1 +1,14 @@
 
+/*
+Problem
+Problem Statement
+The task is very simple: given two integers A and B, write a program to add these two numbers and output it.
+
+Input Format
+The first line contains an integer T, the total number of test cases. Then follow T lines, each line contains two Integers A and B.
+
+Output Format
+For each test case, add A and B and display the sum in a new line.
+
+Everything your program prints is considered “output”, so if you output some debugging statements like “Please enter T”, this will be considered as part of your answer, and because it does not satisfy the output format, it will be marked wrong, even if your answer is otherwise correct!
+*/
