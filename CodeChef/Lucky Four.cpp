8b538d0c14fa95ab68d7138293d4cca7c3cc2703
Abhay Kumar <abhayku2002@gmail.com>
2023-01-28 17:20:59 +0530

    Update Lucky Four.cpp

--- CodeChef/Lucky Four.cpp
+++ CodeChef/Lucky Four.cpp
@@ -13,3 +13,25 @@ Then, there are T lines, each of them contain a single integer from the list.
 Output Format
 Output T lines. Each of these lines should contain the number of occurrences of the digit 4 in the respective integer from Karan's list.
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, count = 0;
+        cin >> n;
+        while (n != 0)
+        {
+            if (n % 10 == 4)
+                count++;
+            n = n / 10;
+        }
+        cout << count << endl;
+    }
+    return 0;
+}
