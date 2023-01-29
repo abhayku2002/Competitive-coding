116ff193c8d11ebc1f65fc4d8c10781f756f1499
Abhay Kumar <abhayku2002@gmail.com>
2023-01-29 23:58:31 +0530

    Update Ada School.cpp

--- CodeChef/Ada School.cpp
+++ CodeChef/Ada School.cpp
@@ -1,4 +1,20 @@
 
+/*
+Ada's classroom contains 
+�
+⋅
+�
+N⋅M tables distributed in a grid with 
+�
+N rows and 
+�
+M columns. Each table is occupied by exactly one student.
+
+Before starting the class, the teacher decided to shuffle the students a bit. After the shuffling, each table should be occupied by exactly one student again. In addition, each student should occupy a table that is adjacent to that student's original table, i.e. immediately to the left, right, top or bottom of that table.
+
+Is it possible for the students to shuffle while satisfying all conditions of the teacher?
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
