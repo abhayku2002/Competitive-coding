24e320aeeb1e57a7b599c1ed7e1972aaa9f6db69
Abhay Kumar <abhayku2002@gmail.com>
2023-01-29 23:24:17 +0530

    Update 67. Add Binary.cpp

--- LeetCode/67. Add Binary.cpp
+++ LeetCode/67. Add Binary.cpp
@@ -1,1 +1,53 @@
 
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        char carry='0';
+        string ans="";
+        int i=a.size()-1;
+        int j=b.size()-1;
+
+        while(i>=0||j>=0){
+            char current='0';
+
+            //Calculating current
+            if(i>=0)
+                {
+                    if(a[i]=='1')
+                    {
+                        current++;                 
+                    }
+                }
+            if(j>=0)
+            {
+                if(b[j]=='1')
+                {
+                    current++;
+                }
+            }
+            if(carry=='1'){
+                current++;
+                carry='0';
+            }
+
+            //Taking actions based on current
+            if(current=='0' || current=='1'){ans+=current;}   
+            else if(current=='2'){
+                ans+='0';
+                carry='1';
+            }
+            else if(current=='3'){
+                ans+='1';
+                carry='1';
+            }
+            i--;
+            j--; 
+        }
+
+        if(carry=='1'){
+            ans+='1';
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
