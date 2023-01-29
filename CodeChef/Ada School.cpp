1dfbecfd4e589415f008856194116214dcb3cda1
Abhay Kumar <abhayku2002@gmail.com>
2023-01-29 23:58:03 +0530

    Update Ada School.cpp

--- CodeChef/Ada School.cpp
+++ CodeChef/Ada School.cpp
@@ -1,1 +1,18 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b;
+        
+        cin>>a>>b;
+        if(a%2==0 || b%2==0)
+            cout<<"YES"<<endl;
+        else 
+            cout<<"NO"<<endl;
+    }
+
+    return 0;
+}
