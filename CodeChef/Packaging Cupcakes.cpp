5c22c6ce384076102db9b14d992a617d4defc327
Abhay Kumar <abhayku2002@gmail.com>
2023-01-30 15:15:20 +0530

    Update Packaging Cupcakes.cpp

--- CodeChef/Packaging Cupcakes.cpp
+++ CodeChef/Packaging Cupcakes.cpp
@@ -1,1 +1,16 @@
 
+/*
+Problem
+Now that Chef has finished baking and frosting his cupcakes, it's time to package them. Chef has N cupcakes,
+and needs to decide how many cupcakes to place in each package. Each package must contain the same number of cupcakes.
+Chef will choose an integer A between 1 and N, inclusive, and place exactly A cupcakes into each package. Chef makes as many packages as possible.
+Chef then gets to eat the remaining cupcakes. Chef enjoys eating cupcakes very much. Help Chef choose the package size A that will let 
+him eat as many cupcakes as possible.
+
+Input
+Input begins with an integer T, the number of test cases. Each test case consists of a single integer N, the number of cupcakes.
+
+Output
+For each test case, output the package size that will maximize the number of leftover cupcakes. If multiple package sizes will result
+in the same number of leftover cupcakes, print the largest such size.
+*/
