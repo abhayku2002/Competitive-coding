b46bf30f1085dec21cdeed43a048dbecd341a481
Abhay Kumar <abhayku2002@gmail.com>
2023-01-31 23:26:09 +0530

    Update Project Euler #1: Multiples of 3 and 5.cpp

--- Hackerrank/Project Euler #1: Multiples of 3 and 5.cpp
+++ Hackerrank/Project Euler #1: Multiples of 3 and 5.cpp
@@ -1,1 +1,9 @@
 
+/*
+f we list all the natural numbers below  that are multiples of  or , we get  and . The sum of these multiples is .
+
+Find the sum of all the multiples of  or  below .
+
+Input Format
+
+First line contains  that denotes the number of test cases. This is followed by  lines, each containing an integer, 
