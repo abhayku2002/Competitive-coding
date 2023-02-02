8f1ea02047b3783b8f016e34b22a21f2ae6fede9
Abhay Kumar <abhayku2002@gmail.com>
2023-02-03 00:28:46 +0530

    Update Functions.cpp

--- Hackerrank/Functions.cpp
+++ Hackerrank/Functions.cpp
@@ -34,3 +34,28 @@ Return the greatest of the four a,b,c,d integers.
 PS: I/O will be automatically handled.
 
 */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int max_of_four(int a,int b,int c,int d){
+    int max = 0;
+    if(a>b && a>c && a>d)
+        max =a;
+    else if (b>a && b>c && b>d)
+        max = b;
+    else if(c>a && c>b && c>d)
+        max = c;
+    else
+        max = d;
+    return max;
+}
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
