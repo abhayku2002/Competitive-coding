8b9d302568510c9903721614032c6f03ce1a9036
Abhay Kumar <abhayku2002@gmail.com>
2023-02-03 00:14:15 +0530

    Update For Loop.cpp

--- Hackerrank/For Loop.cpp
+++ Hackerrank/For Loop.cpp
@@ -16,3 +16,36 @@ for(int i = 0; i < 10; i++) {
 }
 In this challenge, you will use a for loop to increment a variable through a range.
 */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+const char *english[]={"one","two","three","four","five","six","seven","eight","nine"};
+int main() {
+    int a,b;
+    cin>>a>>b;
+    while(b>=a){
+        if((a>=1)&&(a<=9)){
+            cout<<english[a-1]<<endl;
+            }
+        else{
+            if((a%2)==0)
+            
+                cout<<"even"<<endl;
+            
+            else 
+            
+                cout<<"odd"<<endl;
+            }
+                
+        ++a;
+    }
+    return 0;
+}
+
+/*
+int a,b;
+take input a,b;
+int n (interval [a,b]/ b>=a);
+if(1<=n)
+*/
