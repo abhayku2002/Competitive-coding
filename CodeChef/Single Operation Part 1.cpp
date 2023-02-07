1fd813150ea9c0f6266825e238367baf9469f07c
Abhay Kumar <abhayku2002@gmail.com>
2023-02-07 23:06:26 +0530

    Update Single Operation Part 1.cpp

--- CodeChef/Single Operation Part 1.cpp
+++ CodeChef/Single Operation Part 1.cpp
@@ -1,1 +1,38 @@
 
+
+
+/*
+
+Problem
+Chef has the binary representation 
+�
+S of a number 
+�
+X with him. He can modify the number by applying the following operation exactly once:
+
+⊕ denotes the bitwise XOR operation.
+Chef wants to maximize the value of 
+�
+X after performing the operation. Help Chef in determining the value of 
+�
+Y which will maximize the value of 
+�
+X after the operation.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of two lines of inputs - the first containing the length of binary string 
+�
+S.
+The second line of input contains the binary string 
+�
+S.
+Output Format
+For each test case, output on a new line, the value of 
+�
+Y which will maximize the value of 
+�
+X after the operation.
+*/
