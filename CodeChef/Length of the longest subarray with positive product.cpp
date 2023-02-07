b499b78976c0cede50d3a84e193664fac3d76740
Abhay Kumar <abhayku2002@gmail.com>
2023-02-08 00:03:46 +0530

    Update Length of the longest subarray with positive product.cpp

--- CodeChef/Length of the longest subarray with positive product.cpp
+++ CodeChef/Length of the longest subarray with positive product.cpp
@@ -40,3 +40,119 @@ Constraints:
  1<=n<=105
 -109<=arr[i]<=109
 */
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+        // Function to return the length of the
+        //longest subarray with ppositive product
+        int maxLength(vector<int> &arr,int n){
+           //code here
+               int mxleft=0;
+
+           int mxRight=0;
+
+           int neg=0;
+
+           int j=0;
+
+           for(int i=0;i<n;i++)
+
+           {
+
+               if(arr[i]==0){
+
+                   neg=0;
+
+                   j=i+1;
+
+                   continue;
+
+               }
+
+               if(arr[i]<0){
+
+                   neg++;
+
+               }
+
+               if(neg%2==0){
+
+                   mxleft=max(mxleft,i-j+1);
+
+               }
+
+               
+
+           }
+
+           j=n;
+
+           neg=0;
+
+           for(int i=n-1;i>=0;i--)
+
+           {
+
+               if(arr[i]==0){
+
+                   neg=0;
+
+                   j=i;
+
+                   continue;
+
+               }
+
+               if(arr[i]<0){
+
+                   neg++;
+
+               }
+
+               if(neg%2==0){
+
+                   mxRight=max(mxRight,j-i);
+
+               }
+
+               
+
+           }
+
+           return max(mxRight,mxleft);
+
+        
+        }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    vector<int> arr(n);
+	    for(int i = 0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    int res = ob.maxLength(arr,n);
+	    cout<<res<<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
