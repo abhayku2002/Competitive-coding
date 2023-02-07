e42c7b271e4916d6944aa47bc54708ef7703db3e
Abhay Kumar <abhayku2002@gmail.com>
2023-02-07 23:06:38 +0530

    Update Single Operation Part 1.cpp

--- CodeChef/Single Operation Part 1.cpp
+++ CodeChef/Single Operation Part 1.cpp
@@ -36,3 +36,29 @@ Y which will maximize the value of
 �
 X after the operation.
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tc;
+	cin>>tc;
+	while(tc--){
+	    int num;
+	    cin>>num;
+	    string s;
+	    cin>>s;
+	    int count=0;
+	    for(int i=0;i<num;i++){
+	        if(s[i]=='0'){
+	            break;
+	        }
+	        count++;
+	    }
+	    cout<<count<<endl;
+	}
+	return 0;
+}
+
+
