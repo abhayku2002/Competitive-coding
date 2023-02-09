077fcb00970b4b34a11328783fe0a1c3c777a3f8
Abhay Kumar <abhayku2002@gmail.com>
2023-02-09 09:23:43 +0530

    Update Battery Health.cpp

--- CodeChef/Battery Health.cpp
+++ CodeChef/Battery Health.cpp
@@ -23,3 +23,27 @@ For each test case, output on a new line, YES, if the battery is in optimal cond
 
 You may print each character in uppercase or lowercase. For example, NO, no, No and nO, are all considered identical.
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c,x;
+	    cin>>a>>b>>c>>x;
+	    if(a+b >= x)
+	        cout<<"YES"<<endl;
+	    else if(b+c>=x)
+	        cout<<"YES"<<endl;
+	    else if(c+a >= x)
+	        cout<<"YES"<<endl;
+	    else{
+	        cout<<"NO"<<endl;
+	        
+	    }
+	}
+	return 0;
+}
