7d42365328019a75cf185583aca31434f2b086a5
Abhay Kumar <abhayku2002@gmail.com>
2023-02-09 09:25:19 +0530

    Update Netflix.cpp

--- CodeChef/Netflix.cpp
+++ CodeChef/Netflix.cpp
@@ -1,1 +1,35 @@
 
+/*
+Problem
+Alice, Bob, and Charlie are contributing to buy a Netflix subscription. However, Netfix allows only two users to share a subscription.
+
+Given that Alice, Bob, and Charlie have 
+�
+,
+�
+,
+A,B, and 
+�
+C rupees respectively and a Netflix subscription costs 
+�
+X rupees, find whether any two of them can contribute to buy a subscription.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case contains four space-separated integers 
+�
+,
+�
+,
+�
+,
+A,B,C, and 
+�
+X — the amount that Alice, Bob, and Charlie have, and the cost of a Netflix subscription respectively.
+Output Format
+For each test case, output YES, if any two of Alice, Bob, and Charlie can contribute to buy a Netflix subscription or NO otherwise.
+
+You may print each character in uppercase or lowercase. For example, NO, no, No, and nO are all considered identical.
+*/
