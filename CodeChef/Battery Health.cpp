d3c275ef28542a92b425323915ed6d6aa0a0927f
Abhay Kumar <abhayku2002@gmail.com>
2023-02-09 09:23:21 +0530

    Update Battery Health.cpp

--- CodeChef/Battery Health.cpp
+++ CodeChef/Battery Health.cpp
@@ -1,1 +1,25 @@
 
+/*
+Problem
+Apple considers any iPhone with a battery health of 
+80
+%
+80% or above, to be in optimal condition.
+
+Given that your iPhone has 
+�
+%
+X% battery health, find whether it is in optimal condition.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+The first and only line of each test case contains an integer 
+�
+X — the battery health.
+Output Format
+For each test case, output on a new line, YES, if the battery is in optimal condition, and NO otherwise.
+
+You may print each character in uppercase or lowercase. For example, NO, no, No and nO, are all considered identical.
+*/
