3d9e80705bce257270a05350952fa685176b64c3
Abhay Kumar <abhayku2002@gmail.com>
2023-02-10 21:57:52 +0530

    Update Bi_lindrome!.cpp

--- CodeChef/Bi_lindrome!.cpp
+++ CodeChef/Bi_lindrome!.cpp
@@ -1,1 +1,39 @@
 
+/*
+Problem
+You are given a string 
+�
+S of length 
+�
+N.
+
+Your task is to delete a subsequence of maximum length from the string, such that, after concatenating the remaining parts of the string, it becomes a palindrome of length greater than 
+1
+1.
+
+If this is possible, print the maximum length of the subsequence that can be deleted. Otherwise, print 
+−
+1
+−1.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of 
+2
+2 lines of input:
+The first line consists the a single integer 
+�
+N - the length of string 
+�
+S.
+The second line contains string 
+�
+S, consisting of lowercase english alphabets.
+Output Format
+For each test case, if it is possible to delete a subsequence under the given conditions, print a single integer, denoting the maximum length of the subsequence that can be deleted. Otherwise, print 
+−
+1
+−1.
+*/
