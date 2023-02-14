85f4ac0783410d0185190f8183571cce596586c6
Abhay Kumar <abhayku2002@gmail.com>
2023-02-15 00:02:51 +0530

    Update Zero Ones Equal One Zeros.cpp

--- CodeChef/Zero Ones Equal One Zeros.cpp
+++ CodeChef/Zero Ones Equal One Zeros.cpp
@@ -35,3 +35,54 @@ For each test case, output any binary string of length
 N satisfying the given conditions.
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+template<typename... T>
+void see(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+
+#define int long long
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii>
+#define pb push_back
+
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define rev(i,a,b) for(int i=a; i>b; --i)
+#define seea(a,x,y) for(int i=x;i<y;i++){cin>>a[i];}
+#define seev(v,n) for(int i=0;i<n;i++){int x; cin>>x; v.push_back(x);}
+
+const ll inf = INT_MAX;
+const ll mod = 1000000007;
+
+void code()
+{
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	    if(i==0 || i==n-1)
+	        cout<<"1";
+	    else{
+	        cout<<"0";
+	    }
+	    cout<<"\n";
+	
+
+}
+
+signed main()
+{
+	fast;
+	int t = 1;
+	cin >> t;
+	while(t--) code();
+}
