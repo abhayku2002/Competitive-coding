2b8e276e64af99bda738c565cd81109636b6cca2
Abhay Kumar <abhayku2002@gmail.com>
2023-02-14 23:32:23 +0530

    Update Motivation.cpp

--- CodeChef/Motivation.cpp
+++ CodeChef/Motivation.cpp
@@ -1,1 +1,57 @@
 
+/*
+https://www.codechef.com/LP1TO201/problems/IMDB
+
+Problem
+Read problem statements in Russian
+Chef has been searching for a good motivational movie that he can watch during his exam time. His hard disk has 
+�
+X GB of space remaining. His friend has 
+�
+N movies represented with 
+(
+�
+�
+,
+�
+�
+)
+(S 
+i
+​
+ ,R 
+i
+​
+ ) representing (space required, IMDB rating). Help Chef choose the single best movie (highest IMDB rating) that can fit in his hard disk.
+
+Input
+The first line of the input contains a single integer 
+�
+T denoting the number of test cases. The description of 
+�
+T test cases follows.
+The first line of each test case contains two space-separated integers 
+�
+N and 
+�
+X.
+�
+N lines follow. For each valid 
+�
+i, the 
+�
+i-th of these lines contains two space-separated integers 
+�
+�
+S 
+i
+​
+  and 
+�
+�
+R 
+i
+​
+ .
+Output
+For each test case, print a single line containing one integer - the highest rating of an IMDB movie which Chef can store in his hard disk.
