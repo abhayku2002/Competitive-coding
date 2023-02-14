453504acc0ef084c87ccba6c233557c6bf034d51
Abhay Kumar <abhayku2002@gmail.com>
2023-02-15 00:23:58 +0530

    Update Chef and Dolls.cpp

--- CodeChef/Chef and Dolls.cpp
+++ CodeChef/Chef and Dolls.cpp
@@ -1,1 +1,17 @@
+/*
+Problem
+ 
+
+Chef is fan of pairs and he likes all things that come in pairs. He even has a doll collection in which the dolls come in pairs. One day while going through his collection he found that there are odd number of dolls. Someone had stolen a doll!!!
+
+Help chef find which type of doll is missing..
+
+Input
+The first line contains an integer T, the number of test cases.
+The first line of each test case contains an integer N, the number of dolls.
+The next N lines are the types of dolls that are left.
+
+Output
+For each test case, display the type of doll that doesn't have a pair, in a new line.
+*/
 
