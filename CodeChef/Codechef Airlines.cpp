711e902215bf1a541b8cbe933fdf1c25d1854e8b
Abhay Kumar <abhayku2002@gmail.com>
2023-02-17 00:17:29 +0530

    Update Codechef Airlines.cpp

--- CodeChef/Codechef Airlines.cpp
+++ CodeChef/Codechef Airlines.cpp
@@ -1,1 +1,32 @@
 
+/*
+Problem
+Chef has opened a new airline. Chef has 
+10
+10 airplanes where each airplane has a capacity of 
+�
+X passengers.
+On the first day itself, 
+�
+Y people are willing to book a seat in any one of Chef's airplanes.
+
+Given that Chef charges 
+�
+Z rupees for each ticket, find the maximum amount he can earn on the first day.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of three space-separated integers 
+�
+,
+�
+,
+X,Y, and 
+�
+Z — the capacity of each airplane, the number of people willing to book a seat in any one of Chef's airplanes on the first day, and the cost of each seat respectively.
+Output Format
+For each test case, output on a new line, the maximum amount Chef can earn on the first day.
+
+*/
