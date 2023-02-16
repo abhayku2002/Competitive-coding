35b1b96524cb1f0adbad416b84483c6071a6f40f
Abhay Kumar <abhayku2002@gmail.com>
2023-02-16 22:06:09 +0530

    Update Tekken.cpp

--- CodeChef/Tekken.cpp
+++ CodeChef/Tekken.cpp
@@ -41,3 +41,57 @@ For each test case, output on a new line, YES, if you can schedule the fight bet
 You can print each character in uppercase or lowercase. For example NO, no, No, and nO are all considered identical.
 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+template<typename... T>
+void see(T&... args) { ((cin >> args), ...);}
+template<typename... T>
+void put(T&&... args) { ((cout << args << " "), ...);}
+template<typename... T>
+void putl(T&&... args) { ((cout << args << " "), ...); cout<<'\n';}
+
+#define int long long
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define pii pair<int,int>
+#define vi vector<int>
+#define vii vector<pii>
+#define pb push_back
+
+#define rep(i,a,b) for(int i=a; i<b; ++i)
+#define rev(i,a,b) for(int i=a; i>b; --i)
+#define seea(a,x,y) for(int i=x;i<y;i++){cin>>a[i];}
+#define seev(v,n) for(int i=0;i<n;i++){int x; cin>>x; v.push_back(x);}
+
+const ll inf = INT_MAX;
+const ll mod = 1000000007;
+
+void code()
+{
+	
+	int a,b,c;
+	cin>>a>>b>>c;
+	int m= min(b,c);
+	b= b-m;
+	c = c-m;
+	if(a>b && a>c)
+	    cout<<"YES"<<endl;
+	else
+	    
+	    cout<<"NO"<<endl;
+	    
+
+}
+
+signed main()
+{
+	fast;
+	int t = 1;
+	cin >> t;
+	while(t--) code();
+}
+
