9af23ddaea076044048c235ca248bc97c1288e29
Abhay Kumar <abhayku2002@gmail.com>
2023-02-17 00:14:51 +0530

    Update Tom and Jerry Chase.cpp

--- CodeChef/Tom and Jerry Chase.cpp
+++ CodeChef/Tom and Jerry Chase.cpp
@@ -1,1 +1,28 @@
 
+/*
+Problem
+https://www.codechef.com/problems/JERRYCHASE
+
+In a classic chase, Tom is running after Jerry as Jerry has eaten Tom's favourite food.
+
+Jerry is running at a speed of 
+�
+X metres per second while Tom is chasing him at a speed of 
+�
+Y metres per second. Determine whether Tom will be able to catch Jerry.
+
+Note that initially Jerry is not at the same position as Tom.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of two space-separated integers 
+�
+X and 
+�
+Y — the speeds of Jerry and Tom respectively.
+Output Format
+For each test case, output on a new line, YES, if Tom will be able to catch Jerry. Otherwise, output NO.
+
+You can print each character in uppercase or lowercase. For example NO, no, No, and nO are all considered the same.
