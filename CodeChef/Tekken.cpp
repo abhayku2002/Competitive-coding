92d78d6c3fd731db6ed9c50e48d513b2fe6bffa7
Abhay Kumar <abhayku2002@gmail.com>
2023-02-16 22:05:40 +0530

    Update Tekken.cpp

--- CodeChef/Tekken.cpp
+++ CodeChef/Tekken.cpp
@@ -1,4 +1,43 @@
 /*
 https://www.codechef.com/START77D/problems/TEKKEN
 
+Problem
+The ultimate battle has begun in Tekken.
+Each fight has two players. Let the initial health of the players be 
+�
+X and 
+�
+Y, then, after the fight, the health of both the players reduces by 
+min(X,
+ 
+Y)
+min(X, Y).
+
+Anna, Bob, and Claudio have initial health levels of 
+�
+,
+�
+,
+A,B, and 
+�
+C respectively. Each pair of players fight exactly once.
+Considering that you can schedule the fight between each pair in any order, find whether Anna can have a positive health level at the end of the battle.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+The first and only line of each test case contains three space-separated integers 
+�
+,
+�
+,
+A,B, and 
+�
+C — the initial health levels of Anna, Bob, and Claudio respectively.
+Output Format
+For each test case, output on a new line, YES, if you can schedule the fight between each pair in an order such that Anna can have a positive health at the end of the battle. Otherwise, output NO.
+
+You can print each character in uppercase or lowercase. For example NO, no, No, and nO are all considered identical.
+
 */
