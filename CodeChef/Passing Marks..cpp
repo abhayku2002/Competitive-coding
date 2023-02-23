c38852574f169e8c64f565af57ee6a0ef490b81e
Abhay Kumar <abhayku2002@gmail.com>
2023-02-23 22:00:33 +0530

    Update Passing Marks..cpp

--- CodeChef/Passing Marks..cpp
+++ CodeChef/Passing Marks..cpp
@@ -69,3 +69,23 @@ th
 Output Format
 For each test case, output on a new line, the maximum value of the passing mark of the test.
 */
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	    sort(a,a+n);
+	    cout<<a[n-x]-1<<endl;
+	}
+	return 0;
+}
