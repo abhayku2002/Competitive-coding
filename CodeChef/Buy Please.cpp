74bc3f6b5d3c72b83c844f58f359c939434fae71
Abhay Kumar <abhayku2002@gmail.com>
2023-02-26 23:03:20 +0530

    Update Buy Please.cpp

--- CodeChef/Buy Please.cpp
+++ CodeChef/Buy Please.cpp
@@ -3,3 +3,14 @@
 PROBLEM:
 https://www.codechef.com/CCSTART2/problems/BUYPLSE
 */
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,x,y;
+	cin>>a>>b>>x>>y;
+	cout<<a*x+b*y;
+	
+	return 0;
+}
