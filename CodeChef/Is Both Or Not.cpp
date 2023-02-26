da3495634bed8bf9d10b3d3561f33416559dbd11
Abhay Kumar <abhayku2002@gmail.com>
2023-02-26 23:09:45 +0530

    Update Is Both Or Not.cpp

--- CodeChef/Is Both Or Not.cpp
+++ CodeChef/Is Both Or Not.cpp
@@ -4,3 +4,22 @@ PROBLEM:
 
 https://www.codechef.com/CCSTART2/problems/ISBOTH
 */
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin>>n;
+	if((n%5==0) & (n%11==0))
+	    cout<<"BOTH"<<endl;
+	else if(n%5==0)
+        cout<<"ONE"<<endl;
+    else if(n%11==0)
+        cout<<"ONE"<<endl;
+    else{
+        cout<<"NONE"<<endl;
+    }
+	return 0;
+}
