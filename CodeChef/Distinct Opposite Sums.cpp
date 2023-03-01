33074f539ae26b485007e0d77cf2bc7d0f1f2da4
Abhay Kumar <abhayku2002@gmail.com>
2023-03-01 23:16:07 +0530

    Update Distinct Opposite Sums.cpp

--- CodeChef/Distinct Opposite Sums.cpp
+++ CodeChef/Distinct Opposite Sums.cpp
@@ -86,4 +86,27 @@ P of length
 N.
 */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+int t;
+cin>>t;
+while(t--)
+{
+	int n;
+	cin>>n;
+	
+	for(int i = 1;i<=n/2;i++)
+		cout<<i <<" ";
+	for(int i = n;i>n/2;i--)
+		cout<<i <<" ";
+    cout<<endl;
+}
+
+	return 0;
+}
+
+
 
