f505d96b7e0e3a0d34ae4f101d044d0a8b4b9589
Abhay Kumar <abhayku2002@gmail.com>
2023-03-01 23:18:08 +0530

    Create Ageing.cpp

---
+++ CodeChef/Ageing.cpp
@@ -0,0 +1,25 @@
+/*
+Problem
+Chef's current age is 
+20
+20 years, while Chefina's current age is 
+10
+10 years.
+Determine Chefina's age when Chef will be 
+�
+X years old.
+
+Note: Assume that Chef and Chefina were born on same day and same month (just different year).
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of a single integer 
+�
+X, the age of Chef.
+Output Format
+For each test case, output Chefina's age when Chef will be 
+�
+X years old.
+*/
