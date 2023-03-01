94dba797d916df14d69d75d8002223dcc614894f
Abhay Kumar <abhayku2002@gmail.com>
2023-03-01 23:12:07 +0530

    Update One Stone or Two Stones.cpp

--- CodeChef/One Stone or Two Stones.cpp
+++ CodeChef/One Stone or Two Stones.cpp
@@ -1,1 +1,56 @@
 
+/*
+Problem
+Chef and Chefina are playing a game. There are two piles numbered 
+1
+1 and 
+2
+2.
+Pile 
+1
+1 contains 
+�
+X stones while Pile 
+2
+2 contains 
+�
+Y stones.
+
+Chef starts the game.
+
+In his turn, Chef can either remove 
+1
+1 stone each from both the piles or remove 
+2
+2 stones from pile 
+1
+1.
+On the other hand, Chefina, in her turn, can either remove 
+1
+1 stone each from both the piles or remove 
+2
+2 stones from pile 
+2
+2.
+The player who cannot make a move loses. Determine the winner if both players play optimally.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of a single line containing two integers 
+�
+X and 
+�
+Y, the initial number of stones in piles 
+1
+1 and 
+2
+2 respectively.
+Output Format
+For each test case, output CHEF if Chef wins the game, and CHEFINA otherwise.
+
+You may print each character in uppercase or lowercase. For example, the strings CHEF, chef, Chef, and cHEf are all considered identical.
+*/
+
+
