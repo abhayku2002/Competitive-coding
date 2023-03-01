e905b114a2e2be354575b97dc533551c69bbd6cf
Abhay Kumar <abhayku2002@gmail.com>
2023-03-01 23:14:08 +0530

    Update Add smallest prime factor.cpp

--- CodeChef/Add smallest prime factor.cpp
+++ CodeChef/Add smallest prime factor.cpp
@@ -1,1 +1,28 @@
 
+/*
+Problem
+Chef's current score is 
+�
+X. Each second, Chef will find the smallest prime factor of his score and add it to his score.
+
+Determine the minimum time in seconds, after which his score becomes 
+≥
+�
+≥Y.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of a single line containing two integers 
+�
+X and 
+�
+Y, the initial and required score of Chef.
+Output Format
+For each test case, output the minimum time in seconds, after which Chef's score becomes 
+≥
+�
+≥Y.
+*/
+
