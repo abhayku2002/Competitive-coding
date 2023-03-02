0bfe588652e131c557b0c2e786a4d3537dae7e0a
Abhay Kumar <abhayku2002@gmail.com>
2023-03-02 23:04:07 +0530

    Update Saving Taxes.cpp

--- CodeChef/Saving Taxes.cpp
+++ CodeChef/Saving Taxes.cpp
@@ -1,1 +1,32 @@
 
+/*
+https://www.codechef.com/problems/TAXSAVING
+
+Problem
+In Chefland, everyone who earns strictly more than 
+�
+Y rupees per year, has to pay a tax to Chef. Chef has allowed a special scheme where you can invest any amount of money and claim exemption for it.
+
+You have earned 
+�
+X 
+(
+�
+>
+�
+)
+(X>Y) rupees this year. Find the minimum amount of money you have to invest so that you don't have to pay taxes this year.
+
+Input Format
+The first line of input will contain a single integer 
+�
+T, denoting the number of test cases.
+Each test case consists of a single line of input consisting of two space separated integers 
+�
+X and 
+�
+Y denoting the amount you earned and the amount above which you will have to pay taxes.
+Output Format
+For each test case, output a single integer, denoting the minimum amount you need to invest.
+
+*/
