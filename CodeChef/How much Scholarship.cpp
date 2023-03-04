fb1791f70b3dc59c4ae7ee0186fce63880d7368d
Abhay Kumar <abhayku2002@gmail.com>
2023-03-04 12:34:21 +0530

    Update How much Scholarship.cpp

--- CodeChef/How much Scholarship.cpp
+++ CodeChef/How much Scholarship.cpp
@@ -1,1 +1,31 @@
 
+/*
+https://www.codechef.com/problems/ZCOSCH
+
+Problem
+The ZCO Scholarship Contest has just finished, and you finish with a rank of 
+�
+R. You know that Rank 
+1
+1 to Rank 
+50
+50 will get 
+100
+%
+100% scholarship on the ZCO exam fee and Rank 
+51
+51 to Rank 
+100
+100 will get 
+50
+%
+50% percentage scholarship on the ZCO exam fee. The rest do not get any scholarship.
+What percentage of scholarship will you get ?
+
+Input Format
+Input consist of single line of input containing one integer 
+�
+R.
+Output Format
+Output a single line containing one integer — the percentage of scholarship you will get.
+*/
