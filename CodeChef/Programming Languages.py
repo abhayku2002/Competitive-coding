058c902004597473c62c1e2d4dbde43bcf86d7eb
Abhay Kumar <abhayku2002@gmail.com>
2023-03-05 20:22:35 +0530

    Update Programming Languages.py

--- CodeChef/Programming Languages.py
+++ CodeChef/Programming Languages.py
@@ -1,1 +1,26 @@
 
+'''
+
+https://www.codechef.com/problems/PROGLANG
+Problem
+Read problem statements in Mandarin, Bengali, Russian, and Vietnamese as well.
+Chef is a software developer, so he has to switch between different languages sometimes. Each programming language has some features, which are represented by integers here.
+
+Currently, Chef has to use a language with two given features 
+�
+A and 
+�
+B. He has two options --- switching to a language with two features 
+
+ . All four features of these two languages are pairwise distinct.
+
+Tell Chef whether he can use the first language, the second language or neither of these languages (if no single language has all the required features).
+
+Input Format
+The first line of the input contains a single integer 
+�
+T denoting the number of test cases. The description of 
+�
+T test cases follows.
+The first and only line of each test case contains six space-separated integers 
+'''
