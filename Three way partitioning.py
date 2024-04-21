2ed6c52401ae40465af890520d361c65488c7ae2
Abhay Kumar <abhayku2002@gmail.com>
2024-04-21 10:47:36 +0530

    Create Three way partitioning.py

---
+++ Three way partitioning.py
@@ -0,0 +1,17 @@
+'''
+Given an array of size n and a range [a, b]. The task is to partition the array around the range such that the array is divided into three parts.
+1) All elements smaller than a come first.
+2) All elements in range a to b come next.
+3) All elements greater than b appear in the end.
+The individual elements of three sets can appear in any order. You are required to return the modified array.
+
+Note: The generated output is 1 if you modify the given array successfully.
+
+Geeky Challenge: Solve this problem in O(n) time complexity.
+'''
+class Solution:
+    #Function to partition the array around the range such 
+    #that array is divided into three parts.
+	def threeWayPartition(self, array, a, b):
+	    # code here 
+	    array.sort()
